73efb0a9ba3987aea2dce52b6f9db581513821f4
mahaabood99-droid <mahaabood99@gmail.com>
2026-09-19 04:07:51 +0300

    Update music.py

--- music.py
+++ music.py
@@ -600,8 +600,7 @@ background_styles = """
     body.light-mode aside {
         background: rgba(255, 255, 255, 0.95) !important;
         border-right-color: rgba(0, 180, 70, 0.3) !important;
-   css_code = r"""
-css_code = r"""
+  css_code = r"""
 body.light-mode .glass-card,
 body.light-mode div.bg-gradient-to-r.from-\[\#031408\] {
     background: #ffffff !important;
